dadb475b2affd2d574d48d99f71da5ac10f33603
Mina <menady20@gmail.com>
2026-09-21 15:33:23 +0300

    feat(dialog): implement book addition dialog with form and validation

--- script.js
+++ script.js
@@ -13,14 +13,13 @@ function addBookToLibrary(title, author, pages, isRead, imgUrl) {
   const myBook = new Book(title, author, pages, isRead, imgUrl);
   library.push(myBook);
 }
-addBookToLibrary("Atomic Habits", "Mina", 22, false, "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQPS8ehjqvJkVzmFsoX2Eqvix7LAollSLyiuSSdoxQBo2VW-0dPZPOeGJ4&s=10");
-addBookToLibrary("My Library", "Mina", 22, true, "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQPS8ehjqvJkVzmFsoX2Eqvix7LAollSLyiuSSdoxQBo2VW-0dPZPOeGJ4&s=10");
-addBookToLibrary("My Library", "Mina", 22, false, "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQPS8ehjqvJkVzmFsoX2Eqvix7LAollSLyiuSSdoxQBo2VW-0dPZPOeGJ4&s=10");
-addBookToLibrary("My Library", "Mina", 22, true, "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQPS8ehjqvJkVzmFsoX2Eqvix7LAollSLyiuSSdoxQBo2VW-0dPZPOeGJ4&s=10");
-addBookToLibrary("My Library", "Mina", 22, false, "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQPS8ehjqvJkVzmFsoX2Eqvix7LAollSLyiuSSdoxQBo2VW-0dPZPOeGJ4&s=10");
-addBookToLibrary("My Library", "Mina", 22, false, "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQPS8ehjqvJkVzmFsoX2Eqvix7LAollSLyiuSSdoxQBo2VW-0dPZPOeGJ4&s=10");
-addBookToLibrary("My Library", "Mina", 22, true, "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQPS8ehjqvJkVzmFsoX2Eqvix7LAollSLyiuSSdoxQBo2VW-0dPZPOeGJ4&s=10");
-
+// addBookToLibrary("Atomic Habits", "Mina", 22, false, "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQPS8ehjqvJkVzmFsoX2Eqvix7LAollSLyiuSSdoxQBo2VW-0dPZPOeGJ4&s=10");
+// addBookToLibrary("My Library", "Mina", 22, true, "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQPS8ehjqvJkVzmFsoX2Eqvix7LAollSLyiuSSdoxQBo2VW-0dPZPOeGJ4&s=10");
+// addBookToLibrary("My Library", "Mina", 22, false, "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQPS8ehjqvJkVzmFsoX2Eqvix7LAollSLyiuSSdoxQBo2VW-0dPZPOeGJ4&s=10");
+// addBookToLibrary("My Library", "Mina", 22, true, "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQPS8ehjqvJkVzmFsoX2Eqvix7LAollSLyiuSSdoxQBo2VW-0dPZPOeGJ4&s=10");
+// addBookToLibrary("My Library", "Mina", 22, false, "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQPS8ehjqvJkVzmFsoX2Eqvix7LAollSLyiuSSdoxQBo2VW-0dPZPOeGJ4&s=10");
+// addBookToLibrary("My Library", "Mina", 22, false, "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQPS8ehjqvJkVzmFsoX2Eqvix7LAollSLyiuSSdoxQBo2VW-0dPZPOeGJ4&s=10");
+// addBookToLibrary("My Library", "Mina", 22, true, "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQPS8ehjqvJkVzmFsoX2Eqvix7LAollSLyiuSSdoxQBo2VW-0dPZPOeGJ4&s=10");
 const books = document.querySelector(".books");
 function createBook(book) {
   const status = book.isRead ? "read" : "unread";
@@ -47,10 +46,52 @@ function createBook(book) {
 
   books.appendChild(card);
 }
-
-for (const book in library) {
-  if (!Object.hasOwn(library, book)) continue;
-
-  const element = library[book];
-  createBook(element);
+function displayBooks() {
+  for (const book in library) {
+    if (!Object.hasOwn(library, book)) continue;
+    
+    const element = library[book];
+    createBook(element);
+  }
 }
+displayBooks();
+
+
+
+const dialog = document.querySelector("#bookDialog");
+const addBookBtn = document.querySelector("#addBook");
+const cancelBookBtn = document.querySelector("#cancelBook");
+const form = document.querySelector(".book-form");
+
+addBookBtn.addEventListener("click", () => {
+  dialog.showModal();
+});
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const formData = new FormData(form);
+
+  const title = formData.get("title");
+  const author = formData.get("author");
+  const pages = Number(formData.get("pages"));
+  const isRead = formData.get("isRead") === "true";
+  const imgUrl = formData.get("imgUrl");
+
+  const book = new Book(
+    title,
+    author,
+    pages,
+    isRead,
+    imgUrl
+  );
+
+  library.push(book)
+  displayBooks();
+
+  dialog.close();
+});
+
+cancelBookBtn.addEventListener("click", () => {
+  dialog.close();
+});
